9a263ccdd8ae88d019e586b7ca160e328f49b342
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 06:18:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 
+  while i < array.length 
+    yield(array[i])
+  end 
 end
+
+my_each([1,2,3,4]){|name| 
+  puts name
+}
